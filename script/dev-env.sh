c358da3f0966c2e947fcd1d587229746a2c1ceb1
OldRabbit736 <sylvan0212@gmail.com>
2022-09-15 14:00:36 +0900

    설명 추가

--- script/dev-env.sh
+++ script/dev-env.sh
@@ -1,2 +1,6 @@
 # app을 제외한 나머지 서비스(DB 등)를 띄움
 docker compose -f docker-compose-dev.yml --project-name food-reservation-dev up
+
+# app 을 intellij 등 IDE에서 실행하며 다음과 같은 configuration 필요
+# active profile = dev
+# JEP = (비밀번호)
