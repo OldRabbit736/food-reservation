8b244f7cd491a64b6c3cc29c5aec1abf723cd801
OldRabbit736 <sylvan0212@gmail.com>
2022-09-15 11:57:28 +0900

    리팩토링

--- script/dev.sh
+++ script/dev.sh
@@ -1,7 +1,7 @@
 # app과 나머지 서비스(DB 등)를 띄움
 if [ -n "${JEP}" ]; then
   ./gradlew bootJar
-  docker compose -f docker-compose-dev.yml --profile app up --build
+  docker compose -f docker-compose-dev.yml --profile app --project-name food-reservation-dev up --build
 else
   echo "스크립트 실행 전 JEP 환경변수 값을 설정해 주세요!"
 fi
