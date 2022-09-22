7d7149034904eed89810975962eac3c99a520dce
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2022-09-22 20:57:56 +0900

    Infra/ncp and deploy (#20)
    * Add infra
    
    * Add init script resource
    
    * .
    
    * Add script for installing docker in the cloud server
    
    * prod 구동 스크립트 추가
    
    * prod 포트 변경, server script 변경
    
    * .
    
    * .
    
    * .
    
    * Add infra
    
    * Add init script resource
    
    * .
    
    * Add script for installing docker in the cloud server
    
    * prod 구동 스크립트 추가
    
    * prod 포트 변경, server script 변경
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- script/prod.sh
+++ script/prod.sh
@@ -4,10 +4,10 @@
 
 if [ -n "${JEP}" ]; then
 #  임시
-  ./gradlew build
-  docker build -f docker/app/Dockerfile -t oldrabbit736/food-reservation-app:temp .
+#  ./gradlew build
+#  docker build -f docker/app/Dockerfile -t oldrabbit736/food-reservation-app:temp .
 #  임시
-  docker compose -f docker-compose-prod.yml --project-name food-reservation-prod up
+  docker compose -f docker-compose-prod.yml --project-name food-reservation-prod up -d
 else
   echo "스크립트 실행 전 JEP 환경변수 값을 설정해 주세요!"
 fi
