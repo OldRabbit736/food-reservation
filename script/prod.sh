eb791a33db90a6e309a9bc63c5857c5ff1fc28d9
OldRabbit736 <sylvan0212@gmail.com>
2022-09-27 12:02:00 +0900

    .

--- script/prod.sh
+++ script/prod.sh
@@ -1,10 +1,16 @@
 # production server 환경에서 실행되는 스크립트입니다.
 # DockerHub 으로부터 이미지를 다운받아 실행합니다.
-# 스크립트 실행 전 JEP 환경변수 설정이 필요합니다. => export JEP=(비밀번호)
+# 스크립트 실행 전 IMAGE_NAME, JEP 환경변수 설정이 필요합니다. CD 과정에서 자동으로 설정됩니다.
 
-if [ -n "${JEP}" ]; then
-  echo up compose...
-  docker compose -f docker-compose-prod.yml --project-name food-reservation-prod up -d
-else
-  echo "스크립트 실행 전 JEP 환경변수 값을 설정해 주세요!"
+if [ -z "${IMAGE_NAME}" ]; then
+  echo "스크립트 실행 전 IMAGE_NAME 환경변수 값을 설정해 주세요!"
+  exit 1
 fi
+
+if [ -z "${JEP}" ]; then
+  echo "스크립트 실행 전 JEP 환경변수 값을 설정해 주세요!"
+  exit 1
+fi
+
+echo up compose...
+docker compose -f docker-compose-prod.yml --project-name food-reservation-prod up -d
