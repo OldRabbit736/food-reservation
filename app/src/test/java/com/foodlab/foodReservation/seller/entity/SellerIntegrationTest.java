11c4be3dc20eef02457f44dba48b48785a7413ea
OldRabbit736 <sylvan0212@gmail.com>
2022-10-12 22:27:22 +0900

    Singleton Testcontainer 패턴 추가

--- app/src/test/java/com/foodlab/foodReservation/seller/entity/SellerIntegrationTest.java
+++ app/src/test/java/com/foodlab/foodReservation/seller/entity/SellerIntegrationTest.java
@@ -1,17 +1,12 @@
 package com.foodlab.foodReservation.seller.entity;
 
+import com.foodlab.foodReservation.testUtils.AbstractContainerBase;
 import org.hibernate.exception.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.persistence.PersistenceException;
 
@@ -21,19 +16,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
-@ActiveProfiles("test")
-class SellerIntegrationTest {
-
-    @Container
-    static final MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:10.9.2");
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mariadb::getJdbcUrl);
-        registry.add("spring.datasource.username", mariadb::getUsername);
-        registry.add("spring.datasource.password", mariadb::getPassword);
-    }
+class SellerIntegrationTest extends AbstractContainerBase {
 
     @Autowired
     TestEntityManager testEntityManager;
--- app/src/test/java/com/foodlab/foodReservation/seller/repository/SellerRepositoryIntegrationTest.java
+++ app/src/test/java/com/foodlab/foodReservation/seller/repository/SellerRepositoryIntegrationTest.java
@@ -1,16 +1,11 @@
 package com.foodlab.foodReservation.seller.repository;
 
 import com.foodlab.foodReservation.seller.entity.Seller;
+import com.foodlab.foodReservation.testUtils.AbstractContainerBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MariaDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
@@ -19,19 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
-@ActiveProfiles("test")
-class SellerRepositoryIntegrationTest {
-
-    @Container
-    static final MariaDBContainer<?> mariadb = new MariaDBContainer<>("mariadb:10.9.2");
-
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mariadb::getJdbcUrl);
-        registry.add("spring.datasource.username", mariadb::getUsername);
-        registry.add("spring.datasource.password", mariadb::getPassword);
-    }
+class SellerRepositoryIntegrationTest extends AbstractContainerBase {
 
     @Autowired
     SellerRepository sellerRepository;
