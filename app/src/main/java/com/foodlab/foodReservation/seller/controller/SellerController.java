c50955563346cf231d6a3045bb7ad6992b16dedd
OldRabbit736 <sylvan0212@gmail.com>
2022-10-13 22:12:38 +0900

    SellerControllerIntegrationTest 추가

--- app/src/main/java/com/foodlab/foodReservation/seller/controller/SellerController.java
+++ app/src/main/java/com/foodlab/foodReservation/seller/controller/SellerController.java
@@ -4,8 +4,10 @@ import com.foodlab.foodReservation.seller.dto.request.CreateSellerRequest;
 import com.foodlab.foodReservation.seller.dto.response.CreateSellerResponse;
 import com.foodlab.foodReservation.seller.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -16,6 +18,7 @@ public class SellerController {
 
     private final SellerService sellerService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sellers")
     public CreateSellerResponse createSeller(@RequestBody @Valid CreateSellerRequest createSellerRequest) {
         return sellerService.createSeller(createSellerRequest);
